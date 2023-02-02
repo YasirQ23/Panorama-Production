56a2408138c34ad0d254bb8c0032c8e40d34597c
Yasir Qureshi <yasirq23@gmail.com>
2023-02-02 15:22:35 -0500

    css changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
     <div className="footer-container">
       <Container>
         <Row>
-          <Col>
+          <Col className="map-col">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1830178.8443005146!2d-81.20295199240172!3d26.372829399999993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d923c2ef36fb27%3A0xd23ea6eeca6095c9!2sBoca%20Raton%2C%20FL!5e0!3m2!1sen!2sus!4v1675282456034!5m2!1sen!2sus"
               className="mini-map"
